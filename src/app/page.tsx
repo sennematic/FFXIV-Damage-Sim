afb7e6e97eb30d463e6a18541da57b9cf6926519
Sennematic <scott.sennema@gmail.com>
2025-10-21 16:31:35 -0400

    UI updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,6 +50,12 @@ export default function Home() {
           job={jobTest}
           attribute="Critical Hit Rate"
         />
+        <HitRateButton 
+          statValue={454}
+          level={100}
+          job={jobTest}
+          attribute="Direct Hit Rate"
+        />
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
